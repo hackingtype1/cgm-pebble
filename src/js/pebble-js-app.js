5c897a3b246077b5e34d1374dd3ea0211e98cfc4
Domokun14 <hackingtype1@gmail.com>
2014-05-04 14:01:01 -0400

    Settings [configurable] added
    Via the mobile native app, settings can be changed - initial page that
    is linked: http://cgm.azurewebsites.net/pebbleconfig.html - is an
    example, no logic to handle the alert settings is implemented.
    
    My primary reason for the lack of implementation is that I will not
    personally handle the alerts & snooze settings in the pebble app - The
    mobile service, or web service will be doing it. That way, the logic
    can be reused across multiple devices/endpoints - and settings that are
    not device specific are consistent for each user, no matter how they
    are viewing the data.

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -4,6 +4,8 @@ function fetchCgmData(lastReadTime, lastBG) {
 
     var response;
     var req = new XMLHttpRequest();
+    
+    //Make your api call here
     req.open('GET', "https://000.000.000.000/pebbledata, true);
 
     req.onload = function(e) {
@@ -43,6 +45,18 @@ Pebble.addEventListener("appmessage",
                         console.log("Received message: " + JSON.stringify(e.payload));
                         fetchCgmData(e.payload.readtime, e.payload.bg);
                         });
+
+Pebble.addEventListener("showConfiguration", function() {
+                     console.log("showing configuration");
+                     Pebble.openURL('http://cgm.azurewebsites.net/pebbleconfig.html');
+                     });
+             
+Pebble.addEventListener("webviewclosed", function(e) {
+                     console.log("configuration closed");
+                     // webview closed
+                     var options = JSON.parse(decodeURIComponent(e.response));
+                     console.log("Options = " + JSON.stringify(options));
+                     });
 
 
 
